87f78bf8d84da21bf1521e664dd15a42da2e87a1
Collatera24 <kola.tella24@gmail.com>
2024-08-14 15:29:45 +0100

    redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,28 +1,38 @@
 #!/usr/bin/env python3
 '''A module with tools for request caching and tracking.
 '''
-import requests
 import redis
+import requests
+from functools import wraps
 from typing import Callable
 
-class WebCache:
-    def __init__(self):
-        self._redis = redis.Redis()
-    
-    def get_page(self, url: str) -> str:
-        cached_page = self._redis.get(f"cache:{url}")
-        if cached_page:
-            return cached_page.decode('utf-8')
-        
-        response = requests.get(url)
-        page_content = response.text
 
-        self._redis.setex(f"cache:{url}", 10, page_content)
-        self._redis.incr(f"count:{url}")
-        return page_content
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
-# Sample usage
-if __name__ == "__main__":
-    web_cache = WebCache()
-    url = "http://slowwly.robertomurray.co.uk/delay/3000/url/https://www.google.com"
-    print(web_cache.get_page(url))
+
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
+def get_page(url: str) -> str:
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
